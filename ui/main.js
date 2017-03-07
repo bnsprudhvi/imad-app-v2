726f0fa432dde1d749289b94f8ba8bb9c94d8c81
bnsprudhvi <bnsprudhvi66@gmail.com>
2017-03-07 20:33:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,14 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 
 element.innerHTML = 'New Value';
+
+//move the image
+var image=document.getElementById('madi');
+var marginleft=0;
+function moveRight() {
+    marginleft=marginleft+1;
+    img.style.marginleft=marginleft+'px';
+}
+img.onclick=function() {
+    var interval=setInterval(moveRight,50);
+}
